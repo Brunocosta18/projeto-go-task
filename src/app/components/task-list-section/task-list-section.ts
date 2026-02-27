065a5007f45b38f827f040f99a83d554ecc967c5
Bruno <142888087+Brunocosta18@users.noreply.github.com>
2026-02-26 21:43:54 -0300

    Adição dos Modais ao projeto.

--- src/app/components/task-list-section/task-list-section.ts
+++ src/app/components/task-list-section/task-list-section.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskCard } from '../task-card/task-card';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [],
+  imports: [TaskCard],
   templateUrl: './task-list-section.html',
   styleUrl: './task-list-section.css',
 })
-export class TaskListSection {
-
-}
+export class TaskListSection {}
