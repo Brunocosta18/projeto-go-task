c4049f4686e3d0b4c92710c26cbd72ffaefcd8d8
Bruno <142888087+Brunocosta18@users.noreply.github.com>
2026-03-03 06:25:12 -0300

    Ajuste modais parte 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './components/header/header';
 import { MainContent } from './components/main-content/main-content';
 import { TaskFormModal } from "./components/task-form-modal/task-form-modal";
+import { TaskCommentsModal } from "./components/task-comments-modal/task-comments-modal";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, MainContent, TaskFormModal],
+  imports: [Header, MainContent, TaskFormModal, TaskCommentsModal],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
