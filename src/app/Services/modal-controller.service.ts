46ca19be03332f410991ca1dd4f4795bd815e773
Bruno <142888087+Brunocosta18@users.noreply.github.com>
2026-03-09 22:17:27 -0300

    Ajuste dos botoês de fechar modais

--- src/app/Services/modal-controller.service.ts
+++ src/app/Services/modal-controller.service.ts
@@ -16,8 +16,9 @@ export class ModalControllerService {
     private readonly _dialog = inject(Dialog);
 
     openNewTaskModal() {
-       return this._dialog.open(TaskFormModal, {
+       return this._dialog.open<ITaskFormControls>(TaskFormModal, {
             ...this.modalSizeOptions,
+            disableClose: true,
                 data: {
                     mode:'create',
                     formValues: {
@@ -30,8 +31,9 @@ export class ModalControllerService {
     }
 
     openEditTaskModal(formValues: ITaskFormControls) {
-       return this._dialog.open(TaskFormModal, {
+       return this._dialog.open<ITaskFormControls>(TaskFormModal, {
             ...this.modalSizeOptions,
+            disableClose: true,
                 data: {
                     mode:'edit',
                     formValues,
--- src/app/components/task-form-modal/task-form-modal.ts
+++ src/app/components/task-form-modal/task-form-modal.ts
@@ -1,7 +1,8 @@
-import { DIALOG_DATA } from '@angular/cdk/dialog';
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 import { Component, inject } from '@angular/core';
 import { TaskFormModalData } from '../../interfaces/task-form-modal-data.interface';
-import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ITaskFormControls } from '../../interfaces/task-form-controls.interface';
 @Component({
   selector: 'app-task-form-modal',
   imports: [ReactiveFormsModule],
@@ -12,14 +13,18 @@ export class TaskFormModal {
 
 
   readonly _data: TaskFormModalData = inject(DIALOG_DATA);
+  readonly _dialogRef = inject(DialogRef);
 
   taskForm: FormGroup = new FormGroup({
-    name: new FormGroup(this._data.formValues.name,[Validators.required, Validators.minLength(10)]),
-    description: new FormGroup(this._data.formValues.description,[Validators.required, Validators.minLength(10)]),
+    name: new FormControl(this._data.formValues.name,[Validators.required, Validators.minLength(10)]),
+    description: new FormControl(this._data.formValues.description,[Validators.required, Validators.minLength(10)]),
   });
 
   onFormSubmit() {
+    this.closeModal(this.taskForm.value);
+  }
 
-}
-
+  closeModal(formValues: ITaskFormControls | undefined = undefined) {
+    this._dialogRef.close(formValues);
+  }
 }
